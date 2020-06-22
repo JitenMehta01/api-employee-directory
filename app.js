9b4141cce77e32ad056bd24da7d41d44ff9269a0
Jiten Mehta <jitenmehta03@gmail.com>
2020-06-22 11:27:57 +0100

    completed js according to guide. Issues with the displaEmployees function

--- app.js
+++ app.js
@@ -27,4 +27,57 @@ function displayEmployees​ = (employeeData)​{
   employee = employeeData;
 
   let employeeHTML​ = ''
+
+  employees.forEach((employee, index) => {
+    let name = employee.name;
+    let email = employee.email;
+    let city = employee.location.city;
+    let picture = employee.picture;
+
+    employeeHTML​ += `
+    <div class="card" data-index="${index}">
+    <img class="avatar" src="${picture.large}" />
+    <div class="text-container">
+    <h2 class="name">${name.first} ${name.last}</h2>
+    <p class="email">${email}</p>
+    <p class="address">${city}</p>
+    </div>
+    </div>
+    `
+
+    grid.innerHTML = employeeHTML;
+
+  })
+
 }
+
+function displayModel = (index) {
+
+  let {name, dob, phone, email, location, { city, street, state, postcode}, picture} = empployees[index];
+
+  let date = new Date(dob.date);
+
+  const​ modalHTML = ​`
+  <img class="avatar" src="${picture.large}" />
+  <div class="text-container"><h2 class="name">${name.first} ${name.last}</h2>
+  <p class="email">${email}</p><p class="address">${city}</p>
+  <hr />
+  <p>${phone}</p><p class="address">${street}, ${state} ${postcode}</p>
+  <p>Birthday:${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+  </div>  `​;
+
+  overlay.classList.remove(​"hidden"​);
+  modalContainer.innerHTML = modalHTML;
+}
+
+
+grid.addEventListener(click, e =>{
+  if (e.target !== grid){
+    const card = e.target.closest('.card');
+    const index = card.getAttribute('data-index');
+
+    displayModel(index);
+  }
+})
+
+modelClose.addEventListener(​'click'​, () => {  overlay.classList.add(​"hidden"​);});
